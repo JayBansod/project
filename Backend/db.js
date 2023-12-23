742ce382a4d1f6ba73609e6ca6cbd9e90a107f57
WaghAshu03 <waghashutosh03@gmail.com>
2023-12-23 21:26:57 +0530

    Changed all the static url strings for backend on every frontend component to single point BaseURL in src/middleware/auth.jsx

--- Backend/db.js
+++ Backend/db.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
 // const uri = "mongodb://localhost:27017/expenditure";
-const uri = "mongodb+srv://expenditure:expenditure123@expenditure.xikxedj.mongodb.net/?retryWrites=true&w=majority";
+const uri =
+  "mongodb+srv://expenditure:expenditure123@expenditure.xikxedj.mongodb.net/?retryWrites=true&w=majority";
 const connectToDB = () => {
-    try {
-        mongoose.connect(uri);
-        console.log("mongo connected");
-    } catch (err) {
-        console.log(err);
-        process.exit();
-    }
+  try {
+    mongoose.connect(uri);
+    console.log("mongo connected");
+  } catch (err) {
+    console.log(err);
+    process.exit();
+  }
 };
 
 module.exports = connectToDB;
--- src/components/Add.js
+++ src/components/Add.js
@@ -1,121 +1,154 @@
 import React, { useEffect, useState } from "react";
-import { useAuth } from "../middleware/auth";
+import { useAuth, BaseURL } from "../middleware/auth";
+
 function Add() {
-    const { accountData, userData, callUserAuthentication } = useAuth();
-    useEffect(() => {
-        callUserAuthentication();
-    }, []);
-    const userId = userData._id;
+  const { accountData, userData, callUserAuthentication } = useAuth();
+  useEffect(() => {
+    callUserAuthentication();
+  }, []);
+  const userId = userData._id;
 
-    const [transactionInfo, setTransactionInfo] = useState({ userId: userId, from: "", to: "", amount: 0, discription: "" });
-    const [updateAccount, setUpdateAccount] = useState({ userId: userId, from: "", to: "", amount: 0, action: "sub" });
+  const [transactionInfo, setTransactionInfo] = useState({
+    userId: userId,
+    from: "",
+    to: "",
+    amount: 0,
+    discription: "",
+  });
+  const [updateAccount, setUpdateAccount] = useState({
+    userId: userId,
+    from: "",
+    to: "",
+    amount: 0,
+    action: "sub",
+  });
 
-    const onChange = (e) => {
-        let name = e.target.name;
-        let value = e.target.value;
-        setTransactionInfo({ ...transactionInfo, [name]: value });
-        setUpdateAccount({ ...updateAccount, [name]: value });
-        // console.log("transactionInfo", transactionInfo);
-        // console.log("setUpdateAccount", updateAccount);
-    };
+  const onChange = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+    setTransactionInfo({ ...transactionInfo, [name]: value });
+    setUpdateAccount({ ...updateAccount, [name]: value });
+    // console.log("transactionInfo", transactionInfo);
+    // console.log("setUpdateAccount", updateAccount);
+  };
 
-    const onSubmit = async (e) => {
-        e.preventDefault();
+  const onSubmit = async (e) => {
+    e.preventDefault();
 
-        try {
-            // console.log("transactionInfo", transactionInfo);
+    try {
+      // console.log("transactionInfo", transactionInfo);
 
-            // console.log("appdate account add.js", updateAccount);
-            const update = await fetch("http://localhost:5000/api/account/updateAccountBalance", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "Application/json",
-                },
-                body: JSON.stringify(updateAccount),
-            });
-            // console.log("update add.js", update);
-            const response = await fetch("http://localhost:5000/api/account/addTransaction", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "Application/json",
-                },
-                body: JSON.stringify(transactionInfo),
-            });
-            // console.log("transactionInfo response add.js", response);
-            const json = await response.json();
-            if (json.success) {
-                // console.log(json);
-                alert("transcation added");
-            } else {
-                alert("Error");
-            }
-        } catch (error) {
-            console.log(error);
+      // console.log("appdate account add.js", updateAccount);
+      const update = await fetch(
+        BaseURL + "/api/account/updateAccountBalance",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "Application/json",
+          },
+          body: JSON.stringify(updateAccount),
         }
-    };
+      );
+      // console.log("update add.js", update);
+      const response = await fetch(BaseURL + "/api/account/addTransaction", {
+        method: "POST",
+        headers: {
+          "Content-Type": "Application/json",
+        },
+        body: JSON.stringify(transactionInfo),
+      });
+      // console.log("transactionInfo response add.js", response);
+      const json = await response.json();
+      if (json.success) {
+        // console.log(json);
+        alert("transcation added");
+      } else {
+        alert("Error");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    return (
-        <>
-            <div className="container m-5 ">
-                <form onSubmit={onSubmit}>
-                    <div className="d-flex flex-column flex-wrap">
-                        <div className="row1">
-                            <div className="d-flex justify-content-around align-items-center flex-wrap">
-                                <div className="mb-3">
-                                    <label for="accountName" className="form-label">
-                                        From:
-                                    </label>
-                                    <br />
-                                    <select name="from" id="accountName" className="form-select" onChange={onChange}>
-                                        <option selected>Choose...</option>
-                                        {accountData?.map((items, index) => {
-                                            return (
-                                                <>
-                                                    <option value={items.bankName} key={index}>
-                                                        {items.bankName}
-                                                    </option>
-                                                </>
-                                            );
-                                        })}
-                                    </select>
-                                </div>
-                                <div className="mb-3">
-                                    <label for="toStore" className="form-label">
-                                        Store Name
-                                    </label>
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        id="toStore"
-                                        name="to"
-                                        value={transactionInfo.to}
-                                        onChange={onChange}
-                                    />
-                                </div>
-                                <div className="mb-3">
-                                    <label for="amount" className="form-label">
-                                        Amount
-                                    </label>
-                                    <input type="number" className="form-control" id="amount" name="amount" onChange={onChange} />
-                                </div>
-                            </div>
-                        </div>
-                        <div className="row2">
-                            <div className="mb-3">
-                                <label for="discription" className="form-label">
-                                    Discription
-                                </label>
-                                <input type="text" className="form-control " id="discription" name="discription" onChange={onChange} />
-                            </div>
-                        </div>
-                        <div className="row3">
-                            <button className="btn btn-primary">Submit</button>
-                        </div>
-                    </div>
-                </form>
+  return (
+    <>
+      <div className="container m-5 ">
+        <form onSubmit={onSubmit}>
+          <div className="d-flex flex-column flex-wrap">
+            <div className="row1">
+              <div className="d-flex justify-content-around align-items-center flex-wrap">
+                <div className="mb-3">
+                  <label for="accountName" className="form-label">
+                    From:
+                  </label>
+                  <br />
+                  <select
+                    name="from"
+                    id="accountName"
+                    className="form-select"
+                    onChange={onChange}
+                  >
+                    <option selected>Choose...</option>
+                    {accountData?.map((items, index) => {
+                      return (
+                        <>
+                          <option value={items.bankName} key={index}>
+                            {items.bankName}
+                          </option>
+                        </>
+                      );
+                    })}
+                  </select>
+                </div>
+                <div className="mb-3">
+                  <label for="toStore" className="form-label">
+                    Store Name
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="toStore"
+                    name="to"
+                    value={transactionInfo.to}
+                    onChange={onChange}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label for="amount" className="form-label">
+                    Amount
+                  </label>
+                  <input
+                    type="number"
+                    className="form-control"
+                    id="amount"
+                    name="amount"
+                    onChange={onChange}
+                  />
+                </div>
+              </div>
             </div>
-        </>
-    );
+            <div className="row2">
+              <div className="mb-3">
+                <label for="discription" className="form-label">
+                  Discription
+                </label>
+                <input
+                  type="text"
+                  className="form-control "
+                  id="discription"
+                  name="discription"
+                  onChange={onChange}
+                />
+              </div>
+            </div>
+            <div className="row3">
+              <button className="btn btn-primary">Submit</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default Add;
